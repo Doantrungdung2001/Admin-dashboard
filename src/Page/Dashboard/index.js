71860c97d47d4cb859f20ccca804467e5eef35c3
vutuankiet4599 <vutuankiet4599@gmail.com>
2023-05-29 11:51:41 +0700

    add map component (api key quota now is exceeded)

--- src/Page/Dashboard/index.js
+++ src/Page/Dashboard/index.js
@@ -1,11 +1,13 @@
 import { Typography } from "antd";
+import Map from "../Map";
 
-function Dashboard(){
-    return(
+function Dashboard() {
+    return (
         <div>
             <Typography.Text>Dashboard</Typography.Text>
+            <Map />
         </div>
-    )
+    );
 }
 
 export default Dashboard;
---
+++ src/Page/Map.js
@@ -0,0 +1,70 @@
+import React, { useState } from "react";
+import { GoogleMap, Marker, useJsApiLoader } from "@react-google-maps/api";
+import { Button } from "antd";
+
+const containerStyle = {
+    width: "400px",
+    height: "400px",
+};
+
+function Map() {
+    let [center, setCenter] = useState({
+        lat: 20,
+        lng: 105,
+    });
+    const { isLoaded } = useJsApiLoader({
+        id: "google-map-script",
+        googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAP_API_KEY,
+    });
+
+    const [map, setMap] = React.useState(null);
+
+    const onLoad = React.useCallback(
+        function callback(map) {
+            // This is just an example of getting and using the map instance!!! don't just blindly copy!
+            const bounds = new window.google.maps.LatLngBounds(center);
+            map.fitBounds(bounds);
+
+            setMap(map);
+            console.log(center);
+        },
+        [center]
+    );
+
+    const onUnmount = React.useCallback(function callback(map) {
+        setMap(null);
+    }, []);
+
+    const handleMove = () => {
+        navigator.geolocation.getCurrentPosition((posiiton) => {
+            setCenter({
+                lat: posiiton.coords.latitude,
+                lng: posiiton.coords.longitude,
+            });
+        }, null);
+        onLoad(map);
+    };
+    return isLoaded ? (
+        <div>
+            <Button type="primary" onClick={handleMove}>
+                あなたの場所に移動する
+            </Button>
+            <GoogleMap
+                mapContainerStyle={containerStyle}
+                center={center}
+                zoom={10}
+                onLoad={onLoad}
+                onUnmount={onUnmount}
+            >
+                {/* Child components, such as markers, info windows, etc. */}
+                <>
+                    <Marker position={center} />
+                </>
+            </GoogleMap>
+        </div>
+    ) : (
+        <></>
+    );
+}
+
+export default React.memo(Map);
