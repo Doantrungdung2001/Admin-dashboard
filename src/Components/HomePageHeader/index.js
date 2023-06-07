661ccf8ab4be4ff151dfb64dfa26611be2657c78
unknown <lehoa7439lehoa7439@gmail.com>
2023-06-07 13:51:16 +0700

    update navbar

--- src/Components/HomePageHeader/index.js
+++ src/Components/HomePageHeader/index.js
@@ -1,26 +1,29 @@
 import classNames from 'classnames/bind';
 import styles from './HomePageHeader.module.scss';
 import React from 'react';
-import { Button, Input, Layout, Menu, Space } from 'antd';
+import { Button, Input, Layout, Menu, Space, Badge } from 'antd';
 import { Header } from 'antd/es/layout/layout';
-import { BellOutlined, DownOutlined, SearchOutlined, UserOutlined } from '@ant-design/icons';
+import { BellOutlined, DownOutlined, SearchOutlined, UserOutlined, BellFilled } from '@ant-design/icons';
 
 const cx = classNames.bind(styles);
-
+const { Search } = Input;
 function HomePageHeader() {
+    const onSearch = (value) => console.log(value);
     return (
         <>
             <Header className={cx('header')}>
                 <div className={cx('header-block')}>
                     <div className={cx('input-area')}>
-                        <Input addonBefore={<SearchOutlined />} className={cx('input-search')} />
+                        <Search placeholder="喫茶店の名前を探す" onSearch={onSearch} />
                     </div>
                     <div className={cx('information')}>
                         <Space size={60}>
                             <Button type="primary" className={cx('header-btn')}>
                                 客
                             </Button>
-                            <BellOutlined className={cx('header-icon')} />
+                            <Badge count = {15}>
+                                <BellOutlined className={cx('header-icon')} />
+                            </Badge>
                             <div className={cx('profile', 'header-icon')}>
                                 <UserOutlined />
                                 <DownOutlined className={cx('down-icon')} />
