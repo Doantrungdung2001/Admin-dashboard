b7b5230eb88f71c12dc5faf25d4ebcc77208bdca
ToanDL1 <toanlevan0123@gmail.com>
2023-07-05 07:49:06 +0700

    add UserInfo

--- src/Components/HomePageHeader/index.js
+++ src/Components/HomePageHeader/index.js
@@ -1,14 +1,21 @@
 import classNames from 'classnames/bind';
 import styles from './HomePageHeader.module.scss';
-import React from 'react';
+import React, { useContext } from 'react';
 import { Button, Input, Space, Badge } from 'antd';
 import { Header } from 'antd/es/layout/layout';
 import { BellOutlined, DownOutlined, UserOutlined } from '@ant-design/icons';
-import { useSearchParams } from 'react-router-dom';
+import { NavLink, useSearchParams } from 'react-router-dom';
+
+import Tippy from '@tippyjs/react/headless'; // different import path!
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowRightFromBracket, faIdBadge } from '@fortawesome/free-solid-svg-icons';
+import AuthContext from '../AuthContext';
 
 const cx = classNames.bind(styles);
 const { Search } = Input;
 function HomePageHeader() {
+    const authContext = useContext(AuthContext);
+
     const [, setSearchParams] = useSearchParams();
     const onSearch = (value) => setSearchParams({ search: value });
     return (
@@ -27,10 +34,45 @@ function HomePageHeader() {
                                 <BellOutlined className={cx('header-icon')} />
                             </Badge>
 
-                            <div className={cx('profile', 'header-icon')}>
-                                <UserOutlined />
-                                <DownOutlined className={cx('down-icon')} />
-                            </div>
+                            <Tippy
+                                render={(attrs) => (
+                                    <div className="" tabIndex="-1" {...attrs}>
+                                        <div className="card" style={{ width: '10rem' }}>
+                                            <ul className="list-group list-group-flush">
+                                                <NavLink exact to="/user/information">
+                                                    <li
+                                                        className="list-group-item pt-0 pb-0"
+                                                        style={{ color: '#000', textDecoration: 'none' }}
+                                                    >
+                                                        <FontAwesomeIcon
+                                                            icon={faIdBadge}
+                                                            className="mr-1"
+                                                            style={{ width: '16px' }}
+                                                        />
+                                                        <span>Profile</span>
+                                                    </li>
+                                                </NavLink>
+                                                <li className="list-group-item pt-0 pb-0">
+                                                    <FontAwesomeIcon
+                                                        icon={faArrowRightFromBracket}
+                                                        className="mr-1"
+                                                        style={{ width: '16px' }}
+                                                    />{' '}
+                                                    <span>Log out</span>
+                                                </li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                )}
+                                interactive="true"
+                                delay={[0, 1000]}
+                                // placement="bottom-end"
+                            >
+                                <div className={cx('profile', 'header-icon')}>
+                                    <UserOutlined />
+                                    <DownOutlined className={cx('down-icon')} />
+                                </div>
+                            </Tippy>
                         </Space>
                     </div>
                 </div>
--- src/Page/UserInformation/index.js
+++ src/Page/UserInformation/index.js
@@ -1,4 +1,73 @@
+import { faCheck, faMinus, faPenToSquare, faPlus, faXmark } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import axios from 'axios';
+
+import { useContext, useEffect, useState } from 'react';
+import AuthContext from '../../Components/AuthContext';
+import { NavLink } from 'react-router-dom';
+import { Modal } from 'react-bootstrap';
+import { Switch } from 'antd';
+
 function UserInformation() {
+    const authContext = useContext(AuthContext);
+    const [user, setUser] = useState(authContext.currentUser);
+    // const [data, setData] = useState();
+    const [userStores, setUserStores] = useState([]);
+    const [count, setCount] = useState(0);
+    const [response, setResponse] = useState('');
+    const [stateShow, setStateShow] = useState(false);
+
+    const handleData = (e) => {
+        setUser({
+            ...user,
+            [e.target.name]: e.target.value,
+        });
+    };
+    console.log('userChange', user);
+
+    function handleGuest(e, numberGuests) {
+        e.preventDefault();
+        console.log('do day', numberGuests);
+        const updateGuests = async () => {
+            const res = await axios.put(`http://127.0.0.1:8000/api/stores/${user.id}/guests`, { guests: numberGuests });
+            console.log('update guest: ', res);
+            if (res.statusText === 'OK') {
+                setCount((prev) => prev + 1);
+            }
+            console.log(count);
+            return res.data;
+        };
+        updateGuests();
+    }
+
+    useEffect(() => {
+        console.log('call lay coffe');
+        axios
+            .get(`http://127.0.0.1:8000/api/stores?user_id=${user.id}`)
+            .then((response) => {
+                setUserStores(response.data);
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+    }, [count]);
+    // console.log(user);
+    console.log('store', userStores);
+
+    function handleChangeInfo(e) {
+        e.preventDefault();
+        const updateUser = async () => {
+            const res = await axios.put(`http://127.0.0.1:8000/api/users/${user.id}`, { user: user });
+            console.log('res update user :', res.data);
+            setResponse(res.data.message);
+            setStateShow(true);
+            return res.data;
+        };
+
+        updateUser();
+        console.log('updateUser: ', updateUser);
+    }
+
     return (
         <>
             <div className="container p-0">
@@ -7,17 +76,17 @@ function UserInformation() {
                         <div className="row">
                             <div className="card shadow-lg border" style={{ width: '95%' }}>
                                 <div style={{ backgroundColor: '#ccc' }}>
-                                    <h5 class="card-title ml-3 mt-2 mb-2">Card title</h5>
+                                    <h6 className="card-title ml-3 mt-2 mb-2">プロファイルのアバター</h6>
                                 </div>
-                                <div className="card-body ">
+                                <div className="card-body">
                                     <img
                                         src="https://taimienphi.vn/tmp/cf/aut/mAKI-top-anh-dai-dien-dep-chat-1.jpg"
                                         className="d-block mr-auto ml-auto"
-                                        style={{ height: '120px', width: '120px', borderRadius: '100% ' }}
+                                        style={{ height: '160px', width: '160px', borderRadius: '100% ' }}
                                         alt=""
                                     />
-                                    <button href="#" class="btn btn-primary d-block mr-auto ml-auto mt-4">
-                                        Go somewhere
+                                    <button href="#" className="btn btn-primary d-block mr-auto ml-auto mt-4">
+                                        イメジをアップローソ
                                     </button>
                                 </div>
                             </div>
@@ -25,18 +94,46 @@ function UserInformation() {
                         <div className="row mt-3">
                             <div className="card shadow-lg border" style={{ width: '95%' }}>
                                 <div style={{ backgroundColor: '#ccc' }}>
-                                    <h5 class="card-title ml-3 mt-2 mb-2">Card title</h5>
+                                    <h6 className="card-title ml-3 mt-2 mb-2">私の起算点</h6>
                                 </div>
                                 <div className="card-body ">
-                                    <img
-                                        src="https://taimienphi.vn/tmp/cf/aut/mAKI-top-anh-dai-dien-dep-chat-1.jpg"
-                                        className="d-block mr-auto ml-auto"
-                                        style={{ height: '120px', width: '120px', borderRadius: '100% ' }}
-                                        alt=""
-                                    />
-                                    <button href="#" class="btn btn-primary d-block mr-auto ml-auto mt-4">
-                                        Go somewhere
-                                    </button>
+                                    {userStores.map((userStore, index) => {
+                                        return (
+                                            <div className="d-flex justify-content-between" key={index}>
+                                                <div>
+                                                    <p>{userStore.name}</p>
+                                                </div>
+                                                <div>
+                                                    <FontAwesomeIcon className="mr-2" icon={faPenToSquare} />
+                                                    <Switch className="mr-2" defaultChecked="true" />
+                                                    <FontAwesomeIcon
+                                                        className="mr-1 pe-auto"
+                                                        style={{ cursor: 'pointer' }}
+                                                        icon={faPlus}
+                                                        onClick={(e) =>
+                                                            handleGuest(e, userStore.a_working_day.guests + 1)
+                                                        }
+                                                    />
+                                                    <FontAwesomeIcon
+                                                        className="mr-1"
+                                                        style={{ cursor: 'pointer' }}
+                                                        icon={faMinus}
+                                                        onClick={(e) =>
+                                                            handleGuest(e, userStore.a_working_day.guests - 1)
+                                                        }
+                                                    />
+                                                    <span className="pl-1 pr-1" style={{ backgroundColor: '#ccc' }}>
+                                                        {userStore.a_working_day.guests}/{userStore.max_capacity}
+                                                    </span>
+                                                </div>
+                                            </div>
+                                        );
+                                    })}
+                                    <NavLink exact to="/registerDrinkShop">
+                                        <button href="#" className="btn btn-primary d-block mr-auto ml-auto mt-4">
+                                            新しい喫茶店を追加す
+                                        </button>
+                                    </NavLink>
                                 </div>
                             </div>
                         </div>
@@ -44,53 +141,117 @@ function UserInformation() {
                     <div className="col-8 ml-0">
                         <div className="card w-100 shadow-lg border">
                             <div style={{ backgroundColor: '#ccc' }}>
-                                <h5 class="card-title ml-3 mt-2 mb-2">Card title</h5>
+                                <h6 className="card-title ml-3 mt-2 mb-2">アカワントの基本情報</h6>
                             </div>
 
                             <form className="p-3">
-                                <div className="mb-3">
-                                    <label for="exampleInputEmail1" class="form-label">
-                                        Email address
+                                <div className="mb-4">
+                                    <label htmlFor="exampleInputEmail1" className="form-label">
+                                        名前
+                                    </label>
+                                    <input
+                                        type="text"
+                                        className="form-control"
+                                        id="exampleInputEmail1"
+                                        name="name"
+                                        placeholder={user.name}
+                                        onChange={handleData}
+                                    />
+                                </div>
+                                <div className="mb-4">
+                                    <label htmlFor="exampleInputPassword1" className="form-label">
+                                        メルアドルス
                                     </label>
                                     <input
                                         type="email"
                                         className="form-control"
-                                        id="exampleInputEmail1"
-                                        aria-describedby="emailHelp"
+                                        id="exampleInput"
+                                        name="email"
+                                        placeholder={user.email}
+                                        onChange={handleData}
                                     />
-                                    <div id="emailHelp" class="form-text">
-                                        We'll never share your email with anyone else.
-                                    </div>
                                 </div>
-                                <div className="mb-3">
-                                    <label for="exampleInputPassword1" className="form-label">
-                                        Password
-                                    </label>
-                                    <input type="password" className="form-control" id="exampleInputPassword1" />
-                                </div>
-                                <div className="mb-3 d-flex">
+                                <div className="mb-4 d-flex">
                                     <div className="col-6 pl-0">
-                                        <label for="exampleInputPassword1" className="form-label">
-                                            Password
+                                        <label htmlFor="exampleInputPassword1" className="form-label">
+                                            電話番号
                                         </label>
-                                        <input type="password" className="form-control" id="exampleInputPassword1" />
+                                        <input
+                                            type=""
+                                            className="form-control"
+                                            name="phone_num"
+                                            placeholder={user.phone_num}
+                                            onChange={handleData}
+                                        />
                                     </div>
                                     <div className="col-6 pl-0 pr-0">
-                                        <label for="exampleInputPassword1" className="form-label">
-                                            Password
+                                        <label htmlFor="exampleInputPassword1" className="form-label">
+                                            生年月日
                                         </label>
-                                        <input type="password" className="form-control" id="exampleInputPassword1" />
+                                        <input
+                                            type="text"
+                                            className="form-control"
+                                            name="dob"
+                                            placeholder={user.dob}
+                                            onChange={handleData}
+                                        />
                                     </div>
                                 </div>
-                                <button type="submit" className="btn btn-primary">
-                                    Submit
+                                <button className="btn btn-primary" onClick={(e) => handleChangeInfo(e)}>
+                                    ほぞんする
                                 </button>
                             </form>
                         </div>
                     </div>
-                    {/* <div className="col">Column</div> */}
                 </div>
             </div>
+            <Modal show={stateShow} style={{ marginTop: '17%' }}>
+                {response === 'Update Success!' ? (
+                    <div className="modal-content">
+                        <div className="modal-header justify-content-center">
+                            <div>
+                                <FontAwesomeIcon
+                                    icon={faCheck}
+                                    className="text-success"
+                                    style={{ paddingRight: '8px', width: '12px' }}
+                                />
+                                <span>コメント成功</span>
+                            </div>
+                        </div>
+                        <div className="modal-footer justify-content-center">
+                            <button
+                                type="button"
+                                className="btn btn-success"
+                                data-bs-dismiss="modal"
+                                onClick={() => setStateShow(false)}
+                            >
+                                閉じる
+                            </button>
+                        </div>
+                    </div>
+                ) : (
+                    <div className="modal-content">
+                        <div className="modal-header justify-content-center" style={{ alignItems: 'center' }}>
+                            <FontAwesomeIcon
+                                icon={faXmark}
+                                className="text-danger"
+                                style={{ paddingRight: '8px', width: '12px' }}
+                            />
+                            <span>コメント失敗</span>
+                        </div>
+                        <div className="modal-footer justify-content-center">
+                            <button
+                                type="button"
+                                className="btn btn-danger"
+                                data-bs-dismiss="modal"
+                                onClick={() => setStateShow(false)}
+                            >
+                                閉じる
+                            </button>
+                        </div>
+                    </div>
+                )}
+            </Modal>
         </>
     );
 }
